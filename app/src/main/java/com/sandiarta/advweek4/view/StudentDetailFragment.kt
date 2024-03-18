26b31b40161fe6f7f28be956841449e61c6b9ed7
Sandiarta AF <sandiartaaf@gmail.com>
2024-03-18 11:05:33 +0700

    Week 4 HomeWork

--- app/src/main/java/com/sandiarta/advweek4/view/StudentDetailFragment.kt
+++ app/src/main/java/com/sandiarta/advweek4/view/StudentDetailFragment.kt
@@ -5,15 +5,42 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.sandiarta.advweek4.R
+import com.sandiarta.advweek4.databinding.FragmentStudentDetailBinding
+import com.sandiarta.advweek4.databinding.FragmentStudentListBinding
+import com.sandiarta.advweek4.viewmodel.DetailViewModel
+import com.sandiarta.advweek4.viewmodel.ListViewModel
 
 class StudentDetailFragment : Fragment() {
-
+    private lateinit var viewModel: DetailViewModel
+    private lateinit var binding: FragmentStudentDetailBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_student_detail, container, false)
+        binding = FragmentStudentDetailBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel = ViewModelProvider(this).get(DetailViewModel::class.java)
+        viewModel.fetch()
+        observeViewModel()
+
+    }
+
+    fun observeViewModel() {
+        viewModel.studentLD.observe(viewLifecycleOwner, Observer {
+            binding.txtID.setText(it.id)
+            binding.txtName.setText(it.name)
+            binding.txtBod.setText(it.dob)
+            binding.txtPhone.setText(it.phone)
+        })
+
     }
 }
